35a45a56584fc0525c30a6a0865dccd919b22666
Marcel Pennewiß <marcel.pennewiss@fem.tu-ilmenau.de>
2013-03-18 21:37:36 +0000

    updated version with relative identical options in portage now
    git-svn-id: https://subversion.fem.tu-ilmenau.de/repository/fem-overlay/trunk@757 d0c66219-219a-4382-8d04-2ce9737ae069

--- net-misc/ucarp/files/vip-down.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-exec 2> /dev/null
-
-# read in the configuration file
-source /etc/conf.d/ucarp
-
-/sbin/ip addr del $UCARP_VIRTUAL_ADDRESS/$UCARP_VIRTUAL_NETMASK dev $UCARP_VIRTUAL_INTERFACE
-
